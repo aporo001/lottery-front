6c8a5743a1dc8aff5ae080a24d911d2b8c4a35dc
Sukit Chittawanij <sukit.chi@fullteam.tech>
2021-10-27 17:17:56 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import "./App.css";
 import { Button, Container, Box, Typography } from "@material-ui/core";
 import { makeStyles, createStyles } from "@material-ui/core/styles";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useWallet, UseWalletProvider } from "use-wallet";
+import { ethers } from "ethers";
+import { getBalance, deposit, withdrawal } from "./utils/connect-wallet";
+
 const useStyles = makeStyles(() =>
   createStyles({
     userBox: {
@@ -29,6 +32,31 @@ const App = () => {
   const classes = useStyles();
   const [depositBalance, setDepositBalance] = useState(0);
   const wallet = useWallet();
+  const provider =
+    wallet.status === "connected"
+      ? new ethers.providers.Web3Provider(wallet.ethereum)
+      : null;
+
+  const getBal = async () => {
+    const balance = await getBalance(wallet.account, provider);
+    setDepositBalance(balance);
+  };
+  useEffect(() => {
+    if (wallet.status === "connected") {
+      getBal();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [wallet?.status]);
+
+  const depositFn = async () => {
+    await deposit(100, provider);
+    getBal();
+  };
+
+  const withdrawalFn = async () => {
+    await withdrawal(100, provider);
+    getBal();
+  };
 
   return (
     <Container>
@@ -57,6 +85,7 @@ const App = () => {
             color="primary"
             size={"large"}
             className={classes.buyButton}
+            onClick={depositFn}
           >
             Deposit
           </Button>
@@ -65,6 +94,7 @@ const App = () => {
             color="primary"
             size={"large"}
             className={classes.buyButton}
+            onClick={withdrawalFn}
           >
             Withdrawal
           </Button>
--- src/utils/connect-wallet.js
+++ src/utils/connect-wallet.js
@@ -1,4 +1,31 @@
 import { ethers } from "ethers";
+import minichefConfig from "../config/MiniChef.json";
 
-export const provider = new ethers.providers.Web3Provider(window.ethereum);
-export const signer = provider.getSigner();
+const getContract = (provider) =>
+  new ethers.Contract(minichefConfig.address, minichefConfig.abi, provider);
+
+export const getBalance = async (address, provider) => {
+  const contract = getContract(provider);
+  const res = await contract.depositBalance(address);
+  return Number(ethers.utils.formatEther(res));
+};
+
+export const deposit = async (amount, provider) => {
+  const contract = getContract(provider);
+  const contractSigner = contract.connect(provider.getSigner());
+
+  const tx = await contractSigner.deposit(
+    ethers.utils.parseEther(String(amount))
+  );
+
+  await tx.wait();
+};
+
+export const withdrawal = async (amount, provider) => {
+  const contract = getContract(provider);
+  const contractSigner = contract.connect(provider.getSigner());
+  const tx = await contractSigner.withdrawal(
+    ethers.utils.parseEther(String(amount))
+  );
+  await tx.wait();
+};
